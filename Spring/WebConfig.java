3b9dc85afe84257a8663d9f360194268ee985f15
Weave <designjava@naver.com>
2020-07-22 09:18:54 +0900

    Create WebConfig.java

---
+++ Spring/WebConfig.java
@@ -0,0 +1,10 @@
+# WebConfig
+
+```java
+@Configuration
+@ComponentScan(basePackages = "com.baek")
+@ServletComponentScan(basePackages = "com.baek.test")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
+```
